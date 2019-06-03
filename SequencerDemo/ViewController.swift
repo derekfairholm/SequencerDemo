39a57526934832e3d8938196a2f8805f0926d83a
derekfairholm <derek.fairholm@gmail.com>
2019-06-03 12:32:49 -0400

    Project setup

---
+++ SequencerDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SequencerDemo
+//
+//  Created by DEREK FAIRHOLM on 6/3/19.
+//  Copyright © 2019 DEREK FAIRHOLM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
