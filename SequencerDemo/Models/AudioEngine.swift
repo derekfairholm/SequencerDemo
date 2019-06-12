4b2a04c95cf50a52953a55fd6ad368c7a530f9f0
derekfairholm <derek.fairholm@gmail.com>
2019-06-12 14:47:00 -0400

    Finished Demo

---
+++ SequencerDemo/Models/AudioEngine.swift
@@ -0,0 +1,9 @@
+//
+//  AudioEngine.swift
+//  SequencerDemo
+//
+//  Created by DEREK FAIRHOLM on 6/6/19.
+//  Copyright © 2019 DEREK FAIRHOLM. All rights reserved.
+//
+
+import Foundation
---
+++ SequencerDemo/Views/BeatView.swift
@@ -0,0 +1,9 @@
+//
+//  BeatView.swift
+//  SequencerDemo
+//
+//  Created by DEREK FAIRHOLM on 6/12/19.
+//  Copyright © 2019 DEREK FAIRHOLM. All rights reserved.
+//
+
+import Foundation
